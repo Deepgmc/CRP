bda041fbc87fc8cd600e2fe2f91fa8c7b0bb48eb
Deepgmc <deepgmc_job@mail.ru>
2021-10-02 10:38:40 +0300

    init repo

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {VueLoaderPlugin} = require('vue-loader')
+
+const config = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/main.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                use: ['vue-loader']
+            },
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                use: ['vue-loader']
+            },
+            {
+                test: /\.css/,
+                exclude: /node_modules/,
+                use: ['style-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+
+        new HtmlWebpackPlugin({template: 'public/index.html'}),
+
+        new VueLoaderPlugin()
+    ]
+}
+
+module.exports = config
