be787e52d2cf44eb49f89d4d75b86669ad8b2240
Deepgmc <deepgmc_job@mail.ru>
2021-10-14 22:00:20 +0300

    First initialization of vue application

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div v-if="loading">loading</div>
+    <div v-else="loading">loaded</div>
+</template>
+
+
+
+<script>
+export default {
+    data: () => ({
+        loading: true
+    }),
+}
+</script>
+
+
+
+
+<style lang="scss">
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('Main js file');
+import Vue from 'vue'
+import App from './App.vue'
+
+import router from './router/router.js'
+import store from './store/store.js'
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app_container')
